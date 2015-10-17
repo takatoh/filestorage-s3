05660f13813dbc92649af6fe9238a385a2b2674e
takatoh <takatoh.m@gmail.com>
2015-10-17 16:38:29 +0900

    lib/filestorage-d3.rb: Update.

--- lib/filestorage-s3.rb
+++ lib/filestorage-s3.rb
@@ -1,8 +1,3 @@
 require "filestorage/version"
-require "filestorage/local"
-require "filestorage/amazon_s3"
-
-module Filestorage
-  class NotExist < StandardError; end
-  class AlreadyExist < StandardError; end
-end
+require "filestorage-s3/version"
+require "filestorage-s3/amazon_s3"
